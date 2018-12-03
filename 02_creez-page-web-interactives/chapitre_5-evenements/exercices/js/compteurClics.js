cfef0b9fbbf9b2da427a62123060447e9a75c92a
Barbara <barbara.tousverts@gmail.com>
2018-12-03 14:49:43 +0100

    exo 2/4 chap 5

--- 02_creez-page-web-interactives/chapitre_5-evenements/exercices/js/compteurClics.js
+++ 02_creez-page-web-interactives/chapitre_5-evenements/exercices/js/compteurClics.js
@@ -1,1 +1,33 @@
 // alert('Hi');
+
+// Ce que j'ai fait (fonctionne)
+
+function compteClic() {
+    compte++;
+    document.getElementById("compteurClics").textContent = compte;
+}
+function desactiveCompteur (){
+    btnClic.removeEventListener("click", compteClic);
+}
+var compte = 0;
+var btnClic = document.getElementById("clic");
+btnClic.addEventListener("click", compteClic);
+
+var btnDesactive = document.getElementById("desactiver");
+btnDesactive.addEventListener("click", desactiveCompteur);
+// ******************************************************//
+
+// Correction
+
+function clic() {
+    compteurClics++;
+    document.getElementById("compteurClics").textContent = compteurClics;
+}
+
+var compteurClics = 0;
+
+document.getElementById("clic").addEventListener("click", clic);
+
+document.getElementById("desactiver").addEventListener("click", function () {
+    document.getElementById("clic").removeEventListener("click", clic);
+})
