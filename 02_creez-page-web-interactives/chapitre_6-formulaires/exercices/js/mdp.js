1f1ba16b88c0d885ee1532931ec61153d8e4dfff
Barbara <barbara.tousverts@gmail.com>
2018-12-05 15:56:09 +0100

    Fin chap 6

--- 02_creez-page-web-interactives/chapitre_6-formulaires/exercices/js/mdp.js
+++ 02_creez-page-web-interactives/chapitre_6-formulaires/exercices/js/mdp.js
@@ -1,1 +1,41 @@
 // alert('Hi');
+
+// Ce que j'ai fait : fonctionne sauf le regex
+var form = document.querySelector("form");
+form.addEventListener("submit", function (e){
+    var mdp1 = form.elements.mdp1.value;
+    var mdp2 = form.elements.mdp2.value;
+    var regexMdp = [0-9];
+    var aideMdp = document.getElementById("infoMdp");
+
+    if (mdp1 !== mdp2){
+        aideMdp.textContent = "Erreur : les mots de passe ne sont pas identiques.";
+    } else if (mdp1.length < 6 || mdp2.length < 6){
+        aideMdp.textContent = "Erreur : les mots de passe doivent contenir 6 caractères.";
+    } else if (!regexMdp.test(mdp1)){
+        aideMdp.textContent = "Erreur : le mot de passe doit contenir 1 chiffre au minimum";
+    }
+    e.preventDefault();
+});
+
+/********** [CORRECTION] **********/
+// var form = document.querySelector("form");
+// form.addEventListener("submit", function (e) {
+//     var mdp1 = form.elements.mdp1.value;
+//     var mdp2 = form.elements.mdp2.value;
+//     var message = "Mots de passe OK";
+//     if (mdp1 === mdp2) {
+//         if (mdp1.length >= 6) {
+//             var regexMdp = /\d+/;
+//             if (!regexMdp.test(mdp1)) {
+//                 message = "Erreur : le mot de passe ne contient aucun chiffre";
+//             }
+//         } else {
+//             message = "Erreur : la longueur minimale du mot de passe est de 6 caractères";
+//         }
+//     } else {
+//         message = "Erreur : les mots de passe saisis sont différents";
+//     }
+//     document.getElementById("infoMdp").textContent = message;
+//     e.preventDefault();
+// });
--- 02_creez-page-web-interactives/chapitre_6-formulaires/exercices/js/pays.js
+++ 02_creez-page-web-interactives/chapitre_6-formulaires/exercices/js/pays.js
@@ -21,3 +21,41 @@ var listePays = [
     "Autriche",
     "Azerbaïdjan"
 ];
+
+// Ce que j'ai fait
+// document.getElementById("pays").addEventListener("input", function (e){
+//     var pays = e.target.value;
+//     if (e.target.value.indexOf("A") === 0){
+//         for (i=0; i<listePays.length; i++){
+//             var suggestion = document.createElement("div");
+//             suggestion.className = "suggestion";
+//             suggestion.textContent = listePays[i];
+//             document.getElementById("suggestions").appendChild(suggestion);
+//         }
+//     }
+// });
+
+/***** [CORRECTION] *****/
+var paysElt = document.querySelector("input");
+var suggestionsElt = document.getElementById("suggestions");
+
+// Gère le changement de valeur saisie
+paysElt.addEventListener("input", function () {
+    suggestionsElt.innerHTML = ""; // Vidage de la liste des suggestions
+    listePays.forEach(function (pays) {
+        // Si valeur en cours de saisie correspond au début du pays
+        if (pays.indexOf(paysElt.value) === 0) {
+            var suggestionElt = document.createElement("div");
+            suggestionElt.classList.add("suggestion");
+            suggestionElt.textContent = pays;
+            // Gère le lic sur une suggestion
+            suggestionElt.addEventListener("click", function (e) {
+                // Remplacement de la valeur saisie par la suggestion
+                paysElt.value = e.target.textContent;
+                // Vidage de la liste des suggestions
+                suggestionsElt.innerHTML = "";
+            });
+            suggestionsElt.appendChild(suggestionElt);
+        }
+    });
+});
